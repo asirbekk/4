fde35ff5bdcf8c708bd2c0c4ce26125094b59b62
asirbekk <78365807+asirbekk@users.noreply.github.com>
2021-03-17 04:23:47 +0300

    Add files via upload

---
+++ 4-laba.py
@@ -0,0 +1,14 @@
+#1-заданий
+#a = 'barbarian'
+# a = a.replace('bar', 'mur')  # 'murmurian'
+# a = a.replace('mur', 'bur', 1)  # 'burmurian'
+
+# print(a)
+
+
+
+# 2-заданий
+# # a = "Молоко"
+
+# print(a[5:],a[:5])
+# 
