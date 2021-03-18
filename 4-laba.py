4fc033ed1eade3f0ab9e315d37ccf80a8452d369
asirbekk <78365807+asirbekk@users.noreply.github.com>
2021-03-18 17:56:32 +0300

    Update 4-laba.py

--- 4-laba.py
+++ 4-laba.py
@@ -1,14 +1,34 @@
-#1-заданий
-#a = 'barbarian'
-# a = a.replace('bar', 'mur')  # 'murmurian'
-# a = a.replace('mur', 'bur', 1)  # 'burmurian'
+#1-заданий 
 
-# print(a)
+# n = float(input())
+# if n <= 250:
+# 	print(n*7)
+# elif 250<n<300:
+# 	print(n*17)
+# elif n > 300:
+# 	print(n*20)
 
+#2-заданий 
+# a = int(input())
+# b = int(input())
+# c = int(input())
 
-
-# 2-заданий
-# # a = "Молоко"
-
-# print(a[5:],a[:5])
+# if ((a % 2 == 0) and (b % 2 == 0) and (c % 2 == 0)):
+# 	print("Четные число = ", a,b,c,)
+# elif ((a % 2 == 1) or (b % 2 == 0) or (c % 2 == 0)):
+# 	print("Четные число = ","b = ", b, "c = ",c)
+# elif ((a % 2 == 0) or (b % 2 == 1) or (c % 2 == 0)):
+# 	print("Четные число = ", "a = ", a, "c = ",c)
+# elif ((a % 2 == 0) or (b % 2 == 0) or (c % 2 == 1)):
+# 	print("Четные число = ", "a = ", a, "b = ",b)
 # 
+# 	3-заданий 
+# k = int(input(" начинающий число = "))
+# n = int(input(" Конечное число =  "))
+# i = 0
+
+# if n > 9 or n < 0:
+# 	while i <= n:
+# 		print(k ," * ",i," = ", k*i)
+# 		i += 1
+# 		pass	
